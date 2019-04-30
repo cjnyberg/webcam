b28dacb8b6cdfe462c84a20fd3400391cac4bb2c
cjnyberg <40365602+cjnyberg@users.noreply.github.com>
2019-04-30 16:15:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,29 @@ window.onload = function() {
                             navigator.mozGetUserMedia || 
                             navigator.msGetUserMedia);
 
+  // Check that the browser supports getUserMedia.
+// If it doesn't show an alert, otherwise continue.
+if (navigator.getUserMedia) {
+  // Request the camera.
+  navigator.getUserMedia(
+    // Constraints
+    {
+      video: true
+    },
+
+    // Success Callback
+    function(localMediaStream) {
+
+    },
+
+    // Error Callback
+    function(err) {
+      // Log the error to the console.
+      console.log('The following error occurred when trying to use getUserMedia: ' + err);
+    }
+  );
+
+} else {
+  alert('Sorry, your browser does not support getUserMedia');
+}
 }
