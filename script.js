a043c2d3b10d7af19a0e89fcb1614b6895c61d41
cjnyberg <40365602+cjnyberg@users.noreply.github.com>
2019-05-01 09:21:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ var vid = document.getElementById('camera-stream');
 
 // Create an object URL for the video stream and use this 
 // to set the video source.
-vid.src = window.URL.createObjectURL(localMediaStream);
+vid.src = window.URL.createObjectURL(Stream);
 
     },
 
